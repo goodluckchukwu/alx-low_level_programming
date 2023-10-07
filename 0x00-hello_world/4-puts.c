9dfb6c07382ea24d4d0dfb8c544c5d737c036bb6
goodluckchukwu <aluugoodluckchukwu@gmail.com>
2023-10-06 17:33:17 -0700

    Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (success)
+ */
+int main(void)
+
+{
+	puts("\"rogramming is like building a multilingual puzzle");
+return (0);
+}
+
