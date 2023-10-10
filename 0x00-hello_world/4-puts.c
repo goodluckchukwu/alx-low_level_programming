0288f085694284044e9d2ab09656a4e9d30ca5ce
goodluckchukwu <aluugoodluckchukwu@gmail.com>
2023-10-09 17:57:08 -0700

    Hello WorldC

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,9 @@
 /**
  * main - Entry point
  *
- * return: Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
-
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
